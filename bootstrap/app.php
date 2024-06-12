ee64c7e01cf344d0396f403446a902bbb358d797
smaulk <st.mukhametdinov@gmail.com>
2024-06-12 13:33:30 +0500

    commit

---
+++ bootstrap/app.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
