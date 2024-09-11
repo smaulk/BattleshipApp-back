63620a0c5c9e785786eeb225c40d85c302931746
smaulk <st.mukhametdinov@gmail.com>
2024-09-12 00:19:39 +0500

    Сделал JWT аутентификацию

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use App\Http\Services\LoginService;
+use App\Services\LoginService;
 use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
--- app/Services/LoginService.php
+++ app/Services/LoginService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Services;
+namespace App\Services;
 
 use App\Dto\LoginDto;
 use App\Exceptions\HttpException;
