8a7f6b6986758470467cb7519888ceba8810fbd0
smaulk <st.mukhametdinov@gmail.com>
2025-02-24 23:29:42 +0500

    fix

--- app/Services/DeleteGameInvitationService.php
+++ app/Services/DeleteGameInvitationService.php
@@ -7,6 +7,7 @@ use App\Dto\FriendshipDto;
 use App\Exceptions\HttpException;
 use App\Models\GameInvitation;
 use App\Parents\Service;
+use Illuminate\Database\Eloquent\Builder;
 
 final class DeleteGameInvitationService extends Service
 {
@@ -20,10 +21,19 @@ final class DeleteGameInvitationService extends Service
     private function deleteInvite(FriendshipDto $dto): bool
     {
         // Если записи нет, возвращаем true, так как ошибок не возникло
-        if (GameInvitation::findByUsers($dto->friendId, $dto->userId)->doesntExist()) {
+        if ($this->getInvitationsQuery($dto)->doesntExist()) {
             return true;
         }
 
-        return (bool)GameInvitation::findByUsers($dto->friendId, $dto->userId)->delete();
+        return (bool)$this->getInvitationsQuery($dto)->delete();
+    }
+
+    private function getInvitationsQuery(FriendshipDto $dto): Builder
+    {
+        return GameInvitation::query()
+            ->findByUsers($dto->userId, $dto->friendId)
+            ->orWhere(function ($query) use ($dto) {
+                $query->findByUsers($dto->friendId, $dto->userId);
+            });
     }
 }
