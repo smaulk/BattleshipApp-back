01d4f618c10b19c14784215b7f930edbb52f0c7d
smaulk <st.mukhametdinov@gmail.com>
2025-05-04 00:58:35 +0500

    fix DeleteFriendshipService

--- app/Services/DeleteFriendshipService.php
+++ app/Services/DeleteFriendshipService.php
@@ -7,24 +7,30 @@ use App\Dto\FriendshipDto;
 use App\Exceptions\HttpException;
 use App\Models\Friendship;
 use App\Services\Abstract\UsersService;
+use Illuminate\Support\Facades\DB;
 
 final class DeleteFriendshipService extends UsersService
 {
     public function run(FriendshipDto $dto): void
     {
-        if (!$this->deleteFriendship($dto)) {
-            throw new HttpException(500);
-        }
+        DB::transaction(function () use ($dto) {
+            if (!$this->deleteFriendship($dto)) {
+                throw new HttpException(500);
+            }
+
+            (new DeleteGameInvitationService())->run($dto);
+        });
     }
 
     private function deleteFriendship(FriendshipDto $dto): bool
     {
+        /** @var Friendship | null $friendship */
         $friendship = Friendship::findByUsers($dto->userId, $dto->friendId)->first();
         // Если записи нет, возвращаем true, так как ошибок не возникло
         if (!$friendship) {
             return true;
         }
 
-        return $friendship->delete();
+        return (bool)$friendship->delete();
     }
 }
