3fbc1e81af1d2b05ae31829b70862ff78d3c92a8
smaulk <st.mukhametdinov@gmail.com>
2025-05-05 02:02:04 +0500

    fix

--- app/Http/Requests/FinishGameRequest.php
+++ app/Http/Requests/FinishGameRequest.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use App\Dto\EndGameDto;
+use App\Enums\GameType;
 use App\Parents\Request;
+use Illuminate\Validation\Rules\Enum;
 
 final class FinishGameRequest extends Request
 {
     public function rules(): array
     {
         return [
-            'type'   => 'required|int',
+            'type' => ['required', new Enum(GameType::class)],
         ];
     }
 
