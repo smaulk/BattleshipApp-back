defbf49d6f106c8abf7fdb2d53d94b10a9535fe6
smaulk <st.mukhametdinov@gmail.com>
2025-05-16 00:00:13 +0500

    fix

--- app/Http/Requests/GetUserGamesRequest.php
+++ app/Http/Requests/GetUserGamesRequest.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use App\Dto\GetUserGamesDto;
+use App\Enums\GameType;
+use Illuminate\Validation\Rules\Enum;
 
 final class GetUserGamesRequest extends AuthorizedRequest
 {
     public function rules(): array
     {
         return [
-            'startId' => 'nullable|integer',
-            'type'  => 'nullable|integer'
+            'startId' => ['nullable', 'integer'],
+            'type'    => ['nullable', new Enum(GameType::class)]
         ];
     }
 
